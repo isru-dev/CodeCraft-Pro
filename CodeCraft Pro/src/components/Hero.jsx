e1fd7630d836848b98fc85ddb9e8043b5f85e028
israel Gezahegn <isrubest18@gmail.com>
2026-05-04 12:00:22 +0300

    starting building landing page for codecraft education company

---
+++ CodeCraft Pro/src/components/Hero.jsx
@@ -0,0 +1,8 @@
+import './hero.css'
+export function Hero(){
+  return(
+    <div className="hero-container">
+      <p>make ideas real</p>
+    </div>
+  );
+}
---
+++ CodeCraft Pro/src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import { Hero } from "../components/Hero";
+export function Home(){
+  return(
+    <Hero/>
+   );
+}
