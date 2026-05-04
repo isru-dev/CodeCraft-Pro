fd256df703b1136d47f4438227ac8772d995efe0
israel Gezahegn <isrubest18@gmail.com>
2026-05-04 12:31:05 +0300

    building the ui of the hero section

--- CodeCraft Pro/src/components/Hero.jsx
+++ CodeCraft Pro/src/components/Hero.jsx
@@ -2,7 +2,12 @@ import './hero.css'
 export function Hero(){
   return(
     <div className="hero-container">
-      <p>make ideas real</p>
+      <div className="hero-paragraph">
+        <h1>CodeCrafterPro</h1>
+        <h2>Best courses on software</h2>
+        <h3>To change the idea to real worlds application and to solve problem</h3>
+        <button>Signup to see the course</button>
+      </div>
     </div>
   );
 }
